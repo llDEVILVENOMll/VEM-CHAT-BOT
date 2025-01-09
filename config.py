44b7510fbc4a3a831dca9a297329ee608c490f05
VENOM <llDEVILVENOMll@gmail.com>
2025-01-09 14:21:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "81719734c6a0af15e5d35006655c1f84"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "6955568347"))
-SUPPORT_GRP = "DNS_NETWORK"
-UPDATE_CHNL = "Dns_Official_Channel"
-OWNER_USERNAME = "II_RAJPUT_SHIV_OP_II"
+OWNER_ID = int(getenv("OWNER_ID", "7751319212"))
+SUPPORT_GRP = "FRIENSHIP_CLUB_GROUP"
+UPDATE_CHNL = "FEELING_SMILEY"
+OWNER_USERNAME = "lll_DEVIL_VENOM_lll"
